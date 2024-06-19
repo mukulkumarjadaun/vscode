19723b48168e6aa64ea1c421828e64263674c599
MUKUL  KUMAR JADAUN <168295286+mukulkumarjadaun@users.noreply.github.com>
2024-06-20 01:04:03 +0530

    commit2

---
+++ random name .js
@@ -0,0 +1,37 @@
+let random= Math.random()
+let first , second , third
+if(random<0.33){
+    first="me"
+}
+else if ( random >0.33 && random<=0.66){
+    first ="you"
+}
+else{
+    first= "me and you "
+}
+
+
+
+let rando= Math.random()
+if(random<0.33){
+    second="meeeeeee"
+}
+else if ( random >0.33 && random<=0.66){
+    second ="its me"
+}
+else{
+    second= "raunn"
+}
+
+
+let rand= Math.random()
+if(random<0.33){
+    third="same"
+}
+else if ( random >0.33 && random<=0.66){
+    third ="utto"
+}
+else{
+    third= "asku "
+}
+console.log(` ${first} ${second} ${third}`)
